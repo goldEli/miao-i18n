c054449ddc7d5491ddccd6d18bcb0a7b1f852ec0
miaoyu <miaoyu2009@qq.com>
2020-07-08 18:03:33 +0800

    🚀add webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+module.exports = {
+  entry: "./index.js",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "lib"),
+  },
+  node: {
+    fs: "empty",
+  }
+};
